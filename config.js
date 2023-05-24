57559c9bedfac92e34217f205ed46d188b1a48ff
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2023-05-24 08:03:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -33,9 +33,9 @@ global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made with'
 global.author = 'Bot WhatsApp'
-global.alpiskey = '8ecfc216' //daftar sendiri jika key habis https://alpis.eu.org
+global.alpiskey = '3a188cf9' //daftar sendiri jika key habis https://alpis.eu.org
 
-global.btc = 'YOURKEY' //Daftar terlebih dahulu https://api.botcahx.live
+global.btc = 'klegr86M' //Daftar terlebih dahulu https://api.botcahx.live
 global.lann = 'trial' //Note Key Ini Hanya Bertahan Selama 4 Day Sejak Config.js update! lebih baik register di https://api.betabotz.me
 global.APIs = { 
   tio: 'https://api.botcahx.live',
